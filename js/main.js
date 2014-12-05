5e2123da89ca7b0b6b6d5aba80a2071d6bce035a
AntonLecock <anton.lecock@gmail.com>
2014-12-04 21:15:06 -0500

    update main.css, events-list2.html & js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ $(function () {
     var s = skrollr.init({
         forceHeight: false
     });
+
+    $( "#main-navigation" ).hover(function() {
+  	$( "#utility-navigation" ).fadeToggle(100);
+  	$( "#brand" ).toggleClass("passive"); 
+	});
 });
